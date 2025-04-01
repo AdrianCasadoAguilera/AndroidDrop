329eabe09943aca3d7c34f8da967b9d2c1d62a6c
AdrianCasado-IETI <acasadoaguilera.cf@iesesteveterradas.cat>
2025-04-01 17:00:42 +0200

    initial commit

---
+++ android/src/main/java/com/adriancasado/game25/android/AndroidLauncher.java
@@ -0,0 +1,24 @@
+package com.adriancasado.game25.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.adriancasado.game25.Main;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
